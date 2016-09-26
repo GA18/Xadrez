5df7a6bfe1b8c96a984aeb3ab13de39f4fa20159
Gabriel Alves <gabrielalves199816@gmail.com>
2016-09-26 09:23:45 -0300

    Primeiro commit

---
+++ Xadrez Aula/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Xadrez_Aula {
+    class Program {
+
+        static void Main(string[]args) {
+
+
+            Console.Write("Digite o valor do inteiro: ");
+
+            int x = int.Parse(console.ReadLine());
+
+            int y = 2;
+
+            Console.WriteLine("DOBRO = " + Y);
+
+            Console.ReadLine();
+    }
+}
